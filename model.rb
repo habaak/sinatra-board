410ec4700c4be87b4a5b712b71565a33726c3052
habaak <joohn728@gmail.com>
2018-06-12 07:28:35 +0000

    Gemfile modified

--- model.rb
+++ model.rb
@@ -3,7 +3,12 @@ require 'data_mapper' # metagem, requires common plugins too.
 #DataMapper Log 찍기
 DataMapper::Logger.new($stdout,:debug)
 # need install dm-sqlite-adapter
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+configure :development do
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+end
+configure :production do
+  DataMapper::setup(:default, ENV["DATABASE_URL"])
+end
 
 class Post
   include DataMapper::Resource
